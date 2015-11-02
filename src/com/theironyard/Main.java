d5541eca307b9812084477457ab8f1dca7d41561
benjamindrake <spcbdrake@yahoo.com>
2015-11-02 12:00:47 -0500

    initial

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -14,6 +14,15 @@ public class Main {
         stmt.execute("UPDATE players SET health = 50 WHERE name = 'Alice'");
         stmt.execute("DELETE FROM players WHERE name = 'Bob'");
 
+        //BAD
+        //String input = "Charlie";
+        //stmt.execute(String.format("INSERT INTO players VALUES ('%s', 100,10,true)", input));
+        // GOOD
+        String input= "Charlie";
+        PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO players VALUES (?, 100, 10 ,true)");
+        stmt2.setString(1, input;
+        stmt2.execute();
+
         ResultSet results = stmt.executeQuery("SELECT * FROM players");
         while (results.next()) {
             String name = results.getString("name");
